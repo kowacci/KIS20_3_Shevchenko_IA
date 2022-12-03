8a66f7f05161d9f2c83540cce99f1b8ad2bc4bb0
kowacci <ignatwev4enko@mail.ru>
2022-12-03 14:22:38 +0300

    Добавили проверку функции

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import string
-line = string
+
 def replace_symbols(line):
-        string.punctuation
-        line = str(input("Введите строку:"))
-        for i in string.punctuation:
-            if i in line:
-                line = line.replace(i, "")
-        return line
+    string.punctuation
+    for i in string.punctuation:
+        if i in line:
+            line = line.replace(i, "")
+    return line
+line = str(input("Введите строку:"))
+
 print(replace_symbols(line))
--- test.py
+++ test.py
@@ -4,6 +4,12 @@ import unittest
 class TestReplaceSymbols(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(replace_symbols(''), '')
+    def test_abc(self):
+        self.assertEqual(replace_symbols('abc'), 'abc')
+
+    def test_ab(self):
+        self.assertEqual(replace_symbols('a*!&?-_b(){}[]'), 'ab')
 
 if __name__ == '__main__':
     unittest.main()
+
