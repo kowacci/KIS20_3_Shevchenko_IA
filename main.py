4632ed1b7aae7b5f5027ea9fd6cb3094aabb8266
kowacci <ignatwev4enko@mail.ru>
2022-12-03 13:37:45 +0300

    готово

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import string
-
-line = str(input("Введите строку:"))
-string.punctuation
-
-for i in string.punctuation:
-    if i in line:
-        line = line.replace(i, "")
-
-print(line.lower())
-print(type(line))
+line = string
+def replace_symbols(line):
+        string.punctuation
+        line = str(input("Введите строку:"))
+        for i in string.punctuation:
+            if i in line:
+                line = line.replace(i, "")
+        return line
+print(replace_symbols(line))
